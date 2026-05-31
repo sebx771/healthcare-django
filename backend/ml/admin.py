5efbe9ad2ba8e6718fd4aa026345c0de64637218
Sebastian <sebascova18@gmail.com>
2026-05-30 19:46:27 -0500

    feat(ml-admin): añade nuevo campo editable 'default'

--- backend/ml/admin.py
+++ backend/ml/admin.py
@@ -6,8 +6,8 @@ from .models import MetricasModelos
 
 @admin.register(MetricasModelos)
 class MetricasModelosAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre_modelo', 'accuracy','trained_at', 'f1_score', 'ver_archivo_joblib')
-    list_filter = ('trained_at',)
+    list_display = ('id', 'nombre_modelo','default','accuracy','trained_at', 'f1_score', 'ver_archivo_joblib')
+    list_filter = ('trained_at','default')
     search_fields = ('nombre_modelo', 'ruta_archivo_joblib')
 
     ordering = ('-id',)
@@ -26,7 +26,7 @@ class MetricasModelosAdmin(admin.ModelAdmin):
     )
 
     readonly_fields = ('nombre_modelo', 'accuracy', 'precision', 'recall', 'f1_score', 'matriz_confusion', 'ruta_archivo_joblib')
-
+    list_editable = ('default',)
     def ver_archivo_joblib(self, obj):
         """Muestra solo el nombre del archivo en la lista para no saturar la pantalla con la ruta completa"""
         import os
