224767c956b2f5281edcaee0b674ae367bc59e0d
Sebastian <sebascova18@gmail.com>
2026-06-03 08:34:51 -0500

    feat(core): agrega la nueva ruta ml al core

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -20,4 +20,5 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('analytics.urls')),
+    path('api/ml/', include('ml.urls')),
 ]
